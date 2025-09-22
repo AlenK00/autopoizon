f0389b6e57c6e5841c98ed004104f4ba51a86b8d
Alena Karpets <alena.karpets02@mail.ru>
2025-09-23 01:02:05 +0300

    изменение app.py

--- app.py
+++ app.py
@@ -304,6 +304,10 @@ if __name__ == '__main__':
     os.makedirs('static/css', exist_ok=True)
     os.makedirs('static/js', exist_ok=True)
     os.makedirs('static/images/brands', exist_ok=True)
+
+    
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
     
     # Запускаем приложение
-    app.run(debug=True, port=5001)
+    #app.run(debug=True, port=5001)
