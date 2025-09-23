3db15ae1d44b1b629e67343f102ad0c06cc9d232
Alena Karpets <alena.karpets02@mail.ru>
2025-09-23 15:20:52 +0300

    Use port 5001 for Railway

--- app.py
+++ app.py
@@ -308,7 +308,8 @@ if __name__ == '__main__':
     
     #port = int(os.environ.get('PORT', 5000))
     #app.run(host='0.0.0.0', port=port, debug=False)
-    port = int(os.environ.get('PORT', 8080))  # 8080 как fallback
+    
+    port = int(os.environ.get('PORT', 5001))  # 5001 вместо 8080
     app.run(host='0.0.0.0', port=port, debug=False)
     
     # Запускаем приложение
